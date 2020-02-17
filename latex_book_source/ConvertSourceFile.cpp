5086a79309d8cb12b44339c605665f8a3d4aff6d
ngzHappy <zhaixueqiang@hotmail.com>
2020-02-17 10:58:03 +0800

    Update ConvertSourceFile.cpp

--- latex_book_source/ConvertSourceFile.cpp
+++ latex_book_source/ConvertSourceFile.cpp
@@ -2,99 +2,104 @@
 #include <vector>
 #include <list>
 
-namespace sstd_convert_source_file{
+namespace sstd_convert_source_file {
 
     namespace sstd_private {
 
-        enum class StringType{
+        enum class StringType {
             None,
             Left,
             Right,
             Changed,
         };
 
-        class TypedString  {
+        class TypedString {
         public:
-            int start{0};
-            int length{0};
-            StringType type{StringType::None};
-            inline TypedString(int a,int b,StringType c):
+            int start{ 0 };
+            int length{ 0 };
+            StringType type{ StringType::None };
+            inline TypedString(int a, int b, StringType c) :
                 start(a),
                 length(b),
-                type(c){}
+                type(c) {
+            }
         };
 
         inline void convertSourceString(const QString & arg,
-                                 const QString & argLeft,
-                                 const QString & argRight,
-                                 std::vector< TypedString > * argAns){
-            int varLeft{0};
-            for(;;){
-                auto varPos = arg.indexOf(  argLeft,varLeft  );
-                if(varPos<0){/*将最后的部分输出...*/
-                    argAns->emplace_back ( varLeft, arg.size()-varLeft ,StringType::None );
+            const QString & argLeft,
+            const QString & argRight,
+            std::vector< TypedString > * argAns) {
+            int varLeft{ 0 };
+            for (;;) {
+                auto varPos = arg.indexOf(argLeft, varLeft);
+                if (varPos < 0) {/*将最后的部分输出...*/
+                    argAns->emplace_back(varLeft, arg.size() - varLeft, StringType::None);
                     return;
                 }
-                auto varPosEnd =   arg.indexOf( argRight,varLeft  );
-                if(varPos>varLeft){/*将前面的部分输出...*/
-                    argAns->emplace_back( varLeft ,varPos - varLeft,StringType::None );
+                auto varPosEnd = arg.indexOf(argRight, varLeft);
+                if (varPos > varLeft) {/*将前面的部分输出...*/
+                    argAns->emplace_back(varLeft, varPos - varLeft, StringType::None);
                 }
-                argAns->emplace_back(0,0,StringType::Left);
-                argAns->emplace_back(varPos +argLeft.size() ,varPosEnd-varPos-argLeft.size(),StringType::Changed  );
-                argAns->emplace_back(0,0,StringType::Right);
-                varLeft = varPosEnd+argRight.size();
+                argAns->emplace_back(0, 0, StringType::Left);
+                argAns->emplace_back(varPos + argLeft.size(), varPosEnd - varPos - argLeft.size(), StringType::Changed);
+                argAns->emplace_back(0, 0, StringType::Right);
+                varLeft = varPosEnd + argRight.size();
             }
 
         }
 
-        inline QString replaceQStringRef(const QStringRef & arg,const QString &l,const QString &r){
+        inline QString replaceQStringRef(const QStringRef & arg, const QString &l, const QString &r) {
             QRegularExpression const static varRegex{ QStringLiteral(R"(-)") };
             QString varAns = arg.toString();
-            varAns.replace( varRegex,l+QStringLiteral(R"(\hspace{0.05em}\rule[0.52ex]{0.5em}{0.48pt}\hspace{0.05em})")+r );
+            varAns.replace(varRegex, l + QStringLiteral(R"(\hspace{0.05em}\rule[0.52ex]{0.5em}{0.48pt}\hspace{0.05em})") + r);
             return std::move(varAns);
         }
 
     }/**/
 
-QString convertSourceString(const QString & arg_,
-                            const QString & argLeft,
-                            const QString & argRight){
+    QString __convertSourceString(const QString & arg,
+        const QString & argLeft,
+        const QString & argRight) {
 
-    if(arg_.isEmpty()){
-        return {};
-    }
-
-    QString arg = arg_;
-
-    std::vector< sstd_private::TypedString > data;
-    sstd_private::convertSourceString(arg,argLeft,argRight,&data);
-
-    {/*将 //@'^^^^^'@ 替换为 (*@\space*{\fill}@*)// */
-        const QString r1 = QStringLiteral(R"===(//@'^^^^^'@)===");
-        if (arg.indexOf(r1)>-1) {
-            const QString r2 = argLeft +  QStringLiteral(R"===(\hspace*{\fill})===") 
-                +  argRight  +  QStringLiteral(R"==(//)==");
-            arg.replace(r1,r2);
+        if (arg.isEmpty()) {
+            return {};
         }
+
+        std::vector< sstd_private::TypedString > data;
+        sstd_private::convertSourceString(arg, argLeft, argRight, &data);
+
+        {
+            QString ans;
+            for (const auto & varI : data) {
+                if (varI.type == sstd_private::StringType::None) {
+                    ans += sstd_private::replaceQStringRef(QStringRef(&arg, varI.start, varI.length),
+                        argLeft, argRight);
+                } else if (varI.type == sstd_private::StringType::Changed) {
+                    ans += QStringRef(&arg, varI.start, varI.length);
+                } else if (varI.type == sstd_private::StringType::Left) {
+                    ans += argLeft;
+                } else if (varI.type == sstd_private::StringType::Right) {
+                    ans += argRight;
+                }
+            }
+            return std::move(ans);
+        }
+
     }
 
-    {
-        QString ans;
-        for(const auto & varI:data){
-            if(varI.type == sstd_private::StringType::None ){
-                ans += sstd_private::replaceQStringRef( QStringRef(&arg, varI.start,varI.length ),
-                    argLeft,argRight) ;
-            } else if (varI.type == sstd_private::StringType::Changed) {
-                ans +=  QStringRef(&arg, varI.start, varI.length);
-            } else if (varI.type == sstd_private::StringType::Left) {
-                ans += argLeft;
-            }else if(varI.type == sstd_private::StringType::Right){
-                ans += argRight;
+    QString convertSourceString(const QString & arg_,
+        const QString & argLeft,
+        const QString & argRight) {
+        QString arg = arg_;
+        {/*将 //@'^^^^^'@ 替换为 (*@\space*{\fill}@*)// */
+            const QString r1 = QStringLiteral(R"===(//@'^^^^^'@)===");
+            if (arg.indexOf(r1) > -1) {
+                const QString r2 = argLeft + QStringLiteral(R"===(\hspace*{\fill})===")
+                    + argRight + QStringLiteral(R"==(//)==");
+                arg.replace(r1, r2);
             }
         }
-        return std::move(ans);
+        return __convertSourceString(arg,argLeft,argRight);
     }
-
-}
 
 }/**/
